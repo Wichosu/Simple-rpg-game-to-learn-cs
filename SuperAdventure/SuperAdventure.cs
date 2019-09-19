50e468c455e7f42b4aa15491c6549e1e8c6095ee
Wicho <luiscalderonmiranda@gmail.com>
2019-09-18 20:44:52 -0500

    Added labels to the form

--- SuperAdventure/SuperAdventure.cs
+++ SuperAdventure/SuperAdventure.cs
@@ -16,5 +16,10 @@ namespace SuperAdventure
         {
             InitializeComponent();
         }
+
+        private void SuperAdventure_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
