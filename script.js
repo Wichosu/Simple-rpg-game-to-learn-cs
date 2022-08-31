fcd9cafc17d17797a72d899f4b576678eee46ed0
Wicho <luiscalderonmiranda@gmail.com>
2022-08-30 21:49:40 -0500

    pseudo code

--- script.js
+++ script.js
@@ -1,25 +1,12 @@
-/*Factory function to create field object (9 in total)
-gameboar as param
-iterates gameboard rows and gets its fields
-return an array of fields*/
-
 /*Factory function key idea
 Create objects for what they do not what they are*/
 
-/*const createFields = (gameBoard) => {
-  const test = [gameBoard]
-  console.log(test.filter(child => child))
-  //console.log(gameBoard.forEach(child => child.classList? console.log('true'):console.log('false')))
-}*/
+/*field = switch + scratch
+rows = winCondition
+cols = winCondiction
+diagonals = winCondition*/
 
-const gameBoard = 
-  document.getElementById('gameBoard')
-  .children
-  .item(0)
-  .childNodes
+const fields = document.querySelectorAll('#field')
 
-const field = document.querySelectorAll('#field')
-console.log(gameBoard)
-console.log(field)
+console.log(fields)
 
-//createFields(gameBoard)
