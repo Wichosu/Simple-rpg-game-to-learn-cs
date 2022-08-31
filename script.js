0c76ac3794e3cedcc2a83284b61ec6ddcb0c990b
Wicho <luiscalderonmiranda@gmail.com>
2022-08-30 22:38:53 -0500

    switcher function added

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 /*Factory function key idea
 Create objects for what they do not what they are*/
 
-/*field = switch + scratch
+/*field = switcher + scratch + clickable
 rows = winCondition
 cols = winCondiction
 diagonals = winCondition*/
 
+const switcher = (element) => {
+  element.textContent === 'X'? element.textContent = '' : element.textContent = 'X'
+}
+
+
 const fields = document.querySelectorAll('#field')
 
-console.log(fields)
+const newField = switcher(fields[1])
+const secondField = fields[0]
+console.log(newField)
+console.log(secondField)
 
