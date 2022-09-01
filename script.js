0bc2958b6d7dd0aa3cdecc195c076d91d1286795
Wicho <luiscalderonmiranda@gmail.com>
2022-08-31 19:04:24 -0500

    factory function for fields being created

--- script.js
+++ script.js
@@ -6,15 +6,46 @@ rows = winCondition
 cols = winCondiction
 diagonals = winCondition*/
 
-const switcher = (element) => {
-  element.textContent === 'X'? element.textContent = '' : element.textContent = 'X'
+const switcher = {
+  change(status) {
+    return !status
+  }
 }
 
+/*const switcher = (element) => {
+  element.textContent === 'X'? element.textContent = '' : element.textContent = 'X'
+}*/
 
-const fields = document.querySelectorAll('#field')
+const clickable = (element) => {
+  element.style.cursor = 'pointer';
+  element.value = !element.value;
+  console.log(element.value);
+}
 
-const newField = switcher(fields[1])
-const secondField = fields[0]
-console.log(newField)
-console.log(secondField)
+const fields = document.querySelectorAll('#field');
+
+const field = (Switcher) => {
+  const switcher = Object.create(Switcher);
+  const props = {
+    checked: false
+  };
+
+  return {
+    log() {
+      console.log(props.checked);
+    },
+    draw() {
+      props.checked = switcher.change(props.checked)
+    }
+  }
+}
+
+const field1 = field(switcher)
+
+field1.log()
+field1.draw()
+field1.log()
+field1.log()
+field1.draw()
+field1.log()
 
